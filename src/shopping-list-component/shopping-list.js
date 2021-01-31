4f035b7bbd11d8d54dffaf7eab6335d6b0420947
jurij-bozic <48452688+jurij-bozic@users.noreply.github.com>
2021-01-31 16:57:59 +0100

    Commit 2

--- src/shopping-list-component/shopping-list.js
+++ src/shopping-list-component/shopping-list.js
@@ -13,8 +13,7 @@ class ShoppingList extends Component {
     state = {
       list: []
     }
-  
-  
+
     componentDidMount(){
       this.getList();
       this.refreshData();
@@ -28,36 +27,56 @@ class ShoppingList extends Component {
 
   
     refreshData = () => {
-      setInterval(() => this.getList(), 8000);
+      setInterval(() => this.getList(), 30000);
     }
     getList = async () => {
       let data = await api.get('/').then(({data}) => data);
       this.setState({ list: data });
     }
     createItem = async () => {
-      this.getList();
-      let id = (this.state.list.length ? this.state.list.length+1 : 1);
-      let res = await api.post('/', { id, text: '' });
-      this.getList();
+      await api.get('/').then(({data}) => {
+        let id = (data.length ? data.length+1 : 1);
+        api.post('/', { id, text: '', checked: false }).then(() => this.getList());
+      });
+
     }
     deleteItem = async (id) => {
       await api.delete(`/${id}`)
       this.getList();
     }
-    updateItem = async (id, val) => { 
-      await api.patch(`/${id}`, { text: val})    
+    updateItem = async (id, val, checked) => { 
+      if(checked == null){
+        await api.patch(`/${id}`, { text: val})  
+      }
+      else {
+        await api.patch(`/${id}`, { checked: checked})  
+      }
+  
       this.getList()
   }
+  modifyInput(param, id){
+    let itemIndex;
+    this.state.list.map((item, index) => (item.id === id  ? itemIndex = index : ''))
+  
+    let items = [...this.state.list];
+    let item = {...items[itemIndex]};
+    item.text = param;
+    items[itemIndex] = item;
+    this.setState({ list: items });
+  }
+
   
     render() {
   
       return (
         <div className="App">
           <header className="App-header">
-            <button onClick={this.createItem}>createItem</button>
+            <button onClick={this.createItem}>Create Item</button>
             {this.state.list.map(listItem => 
               <div key={listItem.id}>
-                  <input type="text" key={listItem.id} value={listItem.text}  onChange={(value) => this.updateItem(listItem.id, value.target.value)}/>
+                  <input type="checkbox" checked={listItem.checked} onChange={(value) => this.updateItem(listItem.id, listItem.text, value.target.checked)}/>
+                  <input type="text" key={listItem.id} value={listItem.text}  onChange={(value) => this.modifyInput(value.target.value, listItem.id)}/>
+                  <button onClick={() => this.updateItem(listItem.id, listItem.text)}>Create</button>
                   <button onClick={() => this.deleteItem(listItem.id)}>X</button>
               </div>
             )}
@@ -70,3 +89,5 @@ class ShoppingList extends Component {
   }
   
   export default ShoppingList;
+
+  // (value) => this.updateItem(listItem.id, value.target.value)
